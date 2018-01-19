8210cf30be466a2feffd7bfd05d093605ccb6895
mizhanping <1379334572@qq,com>
2018-01-19 14:25:10 +0800

    test

--- src/com/mzp/HelloWorld.java
+++ src/com/mzp/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.mzp;
 public class HelloWorld {
    public static void  main(String args[]){
-       System.out.println("hello world!");
+       System.out.println("hello world11!");
     }
 }
